51a6b6c74d2920733d8618882a03697ebccce27e
tingfeng.zhang <tingfeng.zhang@trafigura.com>
2022-11-04 18:14:19 +0800

    jpa join jpg

--- BootJpa/src/main/java/org/eggsoft/cn/beans/User.java
+++ BootJpa/src/main/java/org/eggsoft/cn/beans/User.java
@@ -18,7 +18,7 @@ public class User implements Serializable{
     private Long id;
     private String name;
 //https://gitlab.global.trafigura.com/CLA/situ/-/blob/master/situ-services/situ-domain/src/main/java/com/trafigura/situ/domain/DocumentInfoEntity.java
-
+///https://blog.csdn.net/Jiangbohao_/article/details/126546997 jpa使用方法
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date dt=new Date();
     @Column(columnDefinition = "Integer default 0")
