8da0f6eef06805b9314a781e1d58d2c60034dd6c
oliver lion <oliverbig@eggsoft.cn>
2022-11-24 07:59:36 +0800

    start caculate min value

---
+++ db/player_dictionary.sql
@@ -0,0 +1,8 @@
+INSERT INTO `player_dictionary` VALUES (64, '2022-11-6 23:01:54', NULL, NULL, NULL, '发挥率', 'fix', '[80,147]');
+INSERT INTO `player_dictionary` VALUES (65, '2022-11-7 21:25:34', NULL, NULL, NULL, '进攻', 'Custome', '1');
+INSERT INTO `player_dictionary` VALUES (66, '2022-11-7 21:42:15', NULL, NULL, NULL, '防守', 'Custome', '1');
+INSERT INTO `player_dictionary` VALUES (67, '2022-11-7 21:43:17', NULL, NULL, NULL, '团队', 'Custome', '1');
+INSERT INTO `player_dictionary` VALUES (68, '2022-11-7 21:43:17', NULL, NULL, NULL, '组织', 'Custome', '0.8');
+INSERT INTO `player_dictionary` VALUES (71, '2022-11-8 20:20:52', NULL, NULL, NULL, '几人一组', 'fix', '4');
+INSERT INTO `player_dictionary` VALUES (73, '2022-11-10 20:22:59', NULL, NULL, NULL, '激情', 'Custome', '0.82');
+INSERT INTO `player_dictionary` VALUES (74, '2022-11-12 09:16:51', NULL, NULL, NULL, '吃瓜群众', 'Custome', '0.81');
