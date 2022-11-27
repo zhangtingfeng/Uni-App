539bc0a1ae712042adca4aa438c40b777c5678d0
oliver lion <oliverbig@eggsoft.cn>
2022-11-27 19:38:55 +0800

    tuokedevmain redis ok

--- BootJpa/src/main/java/org/eggsoft/cn/Controller/IprecordController.java
+++ BootJpa/src/main/java/org/eggsoft/cn/Controller/IprecordController.java
@@ -5,10 +5,13 @@ import org.eggsoft.cn.Service.IprecordService;
 import org.eggsoft.cn.Service.UserService;
 import org.eggsoft.cn.beans.Dictionary;
 import org.eggsoft.cn.beans.Iprecord;
+import org.eggsoft.cn.beans.TeamPlayer;
 import org.eggsoft.cn.beans.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/iprecord")
@@ -16,6 +19,14 @@ public class IprecordController {
 
     @Autowired
     private IprecordService iprecordService;
+
+///http://localhost:58083/api/iprecord/list
+    @GetMapping(value = "/list")
+    public List<Iprecord> querylist() {
+
+        return iprecordService.getAllList();
+
+    }
 
     @RequestMapping(method = RequestMethod.POST, value = "/save")
     synchronized public Iprecord save(@RequestBody Iprecord iprecord) {
--- test04/App.vue
+++ test04/App.vue
@@ -1,4 +1,3 @@
-
 <script>
 	import {
 		mapMutations
@@ -11,39 +10,45 @@
 	export default {
 		onLaunch: function() {
 			// #ifdef H5
-			console.log(
-				`%c hello uniapp %c v${version} `,
-				'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
-				'background:#007aff ;padding: 1px; border-radius: 0 3px 3px 0;  color: #fff; font-weight: bold;'
-			)
-			// #endif
-			// 线上示例使用
-			// console.log('%c uni-app官方团队诚邀优秀前端工程师加盟，一起打造更卓越的uni-app & uniCloud，欢迎投递简历到 hr2013@dcloud.io', 'color: red');
-			console.log('App Launch');
-			// #ifdef APP-PLUS
-			// App平台检测升级，服务端代码是通过uniCloud的云函数实现的，详情可参考：https://ext.dcloud.net.cn/plugin?id=4542
-			if (plus.runtime.appid !== 'HBuilder') { // 真机运行不需要检查更新，真机运行时appid固定为'HBuilder'，这是调试基座的appid
-				checkUpdate()
-			}
+			var paraString = location.search;
+			var paras = paraString.split("=");
+			console.log(paras)
+			let admininvitation = paras[1]
+		   uni.setStorageSync("admin", admininvitation);
+		
+		console.log(
+			`%c hello uniapp %c v${version} `,
+			'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
+			'background:#007aff ;padding: 1px; border-radius: 0 3px 3px 0;  color: #fff; font-weight: bold;'
+		)
+		// #endif
+		// 线上示例使用
+		// console.log('%c uni-app官方团队诚邀优秀前端工程师加盟，一起打造更卓越的uni-app & uniCloud，欢迎投递简历到 hr2013@dcloud.io', 'color: red');
+		console.log('App Launch');
+		// #ifdef APP-PLUS
+		// App平台检测升级，服务端代码是通过uniCloud的云函数实现的，详情可参考：https://ext.dcloud.net.cn/plugin?id=4542
+		if (plus.runtime.appid !== 'HBuilder') { // 真机运行不需要检查更新，真机运行时appid固定为'HBuilder'，这是调试基座的appid
+			checkUpdate()
+		}
 
-			// 一键登录预登陆，可以显著提高登录速度
-			uni.preLogin({
-				provider: 'univerify',
-				success: (res) => {
-					// 成功
-					this.setUniverifyErrorMsg();
-					console.log("preLogin success: ", res);
-				},
-				fail: (res) => {
-					this.setUniverifyLogin(false);
-					this.setUniverifyErrorMsg(res.errMsg);
-					// 失败
-					console.log("preLogin fail res: ", res);
-				}
-			})
-			// #endif
-		},
-		onShow: function() {
+		// 一键登录预登陆，可以显著提高登录速度
+		uni.preLogin({
+			provider: 'univerify',
+			success: (res) => {
+				// 成功
+				this.setUniverifyErrorMsg();
+				console.log("preLogin success: ", res);
+			},
+			fail: (res) => {
+				this.setUniverifyLogin(false);
+				this.setUniverifyErrorMsg(res.errMsg);
+				// 失败
+				console.log("preLogin fail res: ", res);
+			}
+		})
+		// #endif
+	},
+	onShow: function() {
 			console.log('App Show')
 		},
 		onHide: function() {
@@ -63,6 +68,7 @@
 	/* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
 	@import './common/uni.css';
 	@import '@/static/customicons.css';
+
 	/* H5 兼容 pc 所需 */
 	/* #ifdef H5 */
 	@media screen and (min-width: 768px) {
