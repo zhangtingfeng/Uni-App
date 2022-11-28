0348c3c21888177f40b5af29b364e4f6d9afaed9
tingfeng.zhang <tingfeng.zhang@trafigura.com>
2022-11-28 10:00:19 +0800

    rebase main

--- BootJpa/src/main/java/org/eggsoft/cn/Repository/TeamPlayerRepository.java
+++ BootJpa/src/main/java/org/eggsoft/cn/Repository/TeamPlayerRepository.java
@@ -1,12 +1,19 @@
 package org.eggsoft.cn.Repository;
 
 
+import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.eggsoft.cn.beans.TeamPlayer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Tuple;
+import java.awt.print.Pageable;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Repository
 public interface TeamPlayerRepository extends JpaRepository<TeamPlayer, Long> {
@@ -19,6 +26,7 @@ public interface TeamPlayerRepository extends JpaRepository<TeamPlayer, Long> {
     @Query(value = "select ID from player_teamplayer where nickname=? ", nativeQuery = true)
     public List<TeamPlayer> findByNicknameLikeNotDelete(String nickname);
     //@Query(value = "select * from player_teamplayer where nickname=? ", nativeQuery = true)
+
 
 
 }
